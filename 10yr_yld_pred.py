0361be8d0e47373680f37643ece2cc9afa884d14
jakekrafczyk <44145969+jakekrafczyk@users.noreply.github.com>
2019-12-30 23:01:12 -0500

    fixed errors

--- 10yr_yld_pred.py
+++ 10yr_yld_pred.py
@@ -110,9 +110,9 @@ def RollingOLS(X,y,window=int):
         
         #define daterange
         startdate = str(ywin.index[0])
-        startdate = startdate[:-9]
+        startdate = startdate[:-12]
         enddate = str(ywin.index[-1])
-        enddate = enddate[:-9]
+        enddate = enddate[:-12]
         daterange = str(startdate + " to " + enddate)
         dateranges = pd.concat([dateranges, pd.DataFrame([daterange],columns=['Date Range'])],
                                                     axis=0,ignore_index=True)
@@ -181,6 +181,8 @@ def RollingOLS(X,y,window=int):
     return accscores
 
 #run regression on time frame and ratio of choice
-roll_cg_13_weeks = RollingOLS(cg_weekly,y_weekly,window=13)
+roll_cg_13_weeks = RollingOLS(cg_monthly,y_monthly,window=3)
+
+print(roll_cg_13_weeks)
 
 #next - visualize and run predictive analytics
